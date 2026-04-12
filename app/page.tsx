69374f30af53da638ed0f3babbce01c6a86c016e
darlantane <darlan.tane@gmail.com>
2026-04-12 13:59:10 +0200

    update portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
-import About from "@/components/about";
-import Experience from "@/components/experience";
-import Education from "@/components/education";
-import Projects from "@/components/projects";
-import Skills from "@/components/skills";
-import Certifications from "@/components/certifications";
-import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 
 import { Toaster } from "react-hot-toast";
@@ -17,13 +10,6 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto px-4">
         <Hero />
-        <About />
-        <Education />
-        <Certifications />
-        <Projects />
-        <Skills />
-        <Experience />
-        <Contact />
       </div>
       <Footer />
       <Toaster
