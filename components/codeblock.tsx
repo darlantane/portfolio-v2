ca9eebd92463027460f4a624fe1f17e74eb26a8f
darlantane <darlan.tane@gmail.com>
2026-06-05 11:22:38 +0200

    update portfolio

--- components/codeblock.tsx
+++ components/codeblock.tsx
@@ -24,7 +24,7 @@ export default function CodeBlock({ children, className, inline }: any) {
       try {
         const html = await codeToHtml(code, {
           lang: language,
-          theme: 'poimandres',
+          theme: 'nord',
         });
         setHighlightedHtml(html);
       } catch (error) {
